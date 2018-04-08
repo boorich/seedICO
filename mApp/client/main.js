5786ce0d2f8c1405df54620c62fec623f7536c1c
Christian <chrisz0rian@gmail.com>
2018-04-08 11:10:37 +0200

    Github API integration (applied to our repo for testing purposes)

--- mApp/client/main.js
+++ mApp/client/main.js
@@ -6,6 +6,7 @@ import './main.html';
 import './js/web3.min.js';
 
 var self = this;
+var selfT = Template;
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
@@ -14,8 +15,22 @@ Template.hello.onCreated(function helloOnCreated() {
 Template.info.onCreated(function infoOnCreated() {
   // counter starts at 0
 
-  this.issues = new ReactiveVar("");
-  self = this;
+  this.issues = new ReactiveVar("asd");
+  var iss = this.issues;
+  function printRepoCount() {
+    var responseObj = JSON.parse(this.responseText);
+  	iss.set(responseObj);
+  	// alert(self.issues.get());
+  }
+
+
+  var request = new XMLHttpRequest();
+  request.onload = printRepoCount;
+  request.open('get', 'https://api.github.com/repos/empea-careercriminal/seedICO', true)
+  request.send();
+
+  //selfT.instance().issues.set(self.issues.get());;
+  this.issues.set(this.issues.get());
 });
 
 Template.hello.helpers({
@@ -23,6 +38,19 @@ Template.hello.helpers({
     return Template.instance().counter.get();
   },
 });
+
+Template.info.helpers({
+  issues() {
+    return Template.instance().issues.get();
+  },
+});
+
+  Template.hello.events({
+    'click button'(event, instance) {
+      // increment the counter when button is clicked
+      selfT.instance().counter.set(instance.counter.get() + 1);
+    },
+  });
 
 
 
@@ -34,27 +62,3 @@ web3.eth.defaultAccount = web3.eth.accounts[0];
 //alert(web3.fromWei(web3.eth.getBalance("0xb90b504f4c6bd7535a0bcd200b1a7c17d1cd4f64")), "ether");
 
 //Github API
-
-function printRepoCount() {
-  var responseObj = JSON.parse(this.responseText);
-	self.issues.set("asd");
-	alert(self.issues.get());
-
-}
-
-
-var request = new XMLHttpRequest();
-request.onload = printRepoCount;
-request.open('get', 'https://api.github.com/repos/empea-careercriminal/seedICO', true)
-request.send()
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-
-
-
-
